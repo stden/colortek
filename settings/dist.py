6f32f36e4d86d1c83a34e5351f8dea283c546e5c
Даниил Гусев <dangusev92@gmail.com>
2013-05-19 23:57:07 +0400

    timelog

--- settings/dist.py
+++ settings/dist.py
@@ -106,7 +106,7 @@ TEMPLATE_CONTEXT_PROCESSORS = (
     'django.contrib.auth.context_processors.auth',
     'django.contrib.messages.context_processors.messages',
     'apps.core.context_processors.global_settings',
-    'apps3.core.context_processors.global_referer',
+    'apps.core.context_processors.global_referer',
     'apps.core.context_processors.template',
     'apps.catalog.context_processors.cart',
     'apps.catalog.context_processors.services',
