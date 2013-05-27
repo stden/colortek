7375c4ed3967210f47bd24dc8b962368de533c19
Даниил Гусев <dangusev92@gmail.com>
2013-05-27 17:37:17 +0400

    add django.core.context_processors.request in dist.py

--- settings/dist.py
+++ settings/dist.py
@@ -118,7 +118,6 @@ TEMPLATE_LOADERS = (
 # )
 
 TEMPLATE_CONTEXT_PROCESSORS = global_settings.TEMPLATE_CONTEXT_PROCESSORS + (
-    'django.core.context_processors.request',
     'apps.core.context_processors.global_settings',
     'apps.core.context_processors.global_referer',
     'apps.catalog.context_processors.cart',
@@ -128,6 +127,8 @@ TEMPLATE_CONTEXT_PROCESSORS = global_settings.TEMPLATE_CONTEXT_PROCESSORS + (
     # 'apps.accounts.context_processors.stats',
     'apps.geo.context_processors.geoip',
     'apps.core.context_processors.template',
+    'django.core.context_processors.request',
+
 
 )
 MIDDLEWARE_CLASSES = (
