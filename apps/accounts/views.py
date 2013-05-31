5993c0118a5dbecbeb4be509430d5ac8a01f866d
Даниил Гусев <dangusev92@gmail.com>
2013-05-31 12:53:26 +0400

    catalog.models.get_item_list check if None

--- apps/accounts/views.py
+++ apps/accounts/views.py
@@ -74,8 +74,10 @@ def register_user(request):
             user.is_active = True
             user.is_verified = True
             user.save()
-            auth.login(request, user)
-        #    auth_user = auth.authenticate(email=user.email, password = form.cleaned_data['password'])
+            auth_user = auth.authenticate(email=user.email, password = form.cleaned_data['password'])
+            print auth_user
+            auth.login(request, auth_user)
+
         #    auth.login(request, auth_user)
 
             # process invite
