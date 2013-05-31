2ffcb82f9d3bb38842b17e91aaa577a1357d1ba8
Даниил Гусев <dangusev92@gmail.com>
2013-05-31 12:23:35 +0400

    Баг 4 васильев

--- apps/catalog/models.py
+++ apps/catalog/models.py
@@ -872,6 +872,8 @@ class Order(models.Model):
 
 
     def get_item_title_list(self):
+        for product in self.containers.all():
+            print product
         return [i.product.title for i in self.containers.all()]
 
     def get_item_titles(self):
