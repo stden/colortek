ffe9c40357c9b37a66d974ba0b6d2be935346491
Даниил Гусев <dangusev92@gmail.com>
2013-06-11 16:52:25 +0400

    geoip fixed

--- apps/geo/context_processors.py
+++ apps/geo/context_processors.py
@@ -27,7 +27,7 @@ class GeoIPLookupDict(object):
         raise TypeError("GeoIPLookupDict is not iterable")
 
 def geoip(request):
-    ip = request.META.get('HTTP_X_FORWARDED_FOR', '')
+    ip = request.META.get('HTTP_X_FORWARDED_FOR', request.META.get('REMOTE_ADDR'))
     if ip:
         ip = ip.split(',')[0]
     geop = IPGeoBase.objects.by_ip(ip)
